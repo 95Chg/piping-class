f70d8d21622483d3e395a99adc187e213b60ac7d
Hyeon-Gyu <rellic95@gmail.com>
2022-11-02 22:42:12 +0900

    initial setting

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <div>개발 중</div>
+    </div>
+  );
+}
+
+export default App;
